4c81f6b22842f15e8d60fa610a41a4aa176834f0
Sakshi <sakshi_s@epam.com>
2025-03-10 14:46:39 +0530

    third Commit

--- task03/jsrc/main/java/com/task03/HelloWorld.java
+++ task03/jsrc/main/java/com/task03/HelloWorld.java
@@ -2,6 +2,7 @@ package com.task03;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
 
@@ -9,19 +10,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 @LambdaHandler(
-    lambdaName = "hello_world",
-	roleName = "hello_world-role",
-	isPublishVersion = true,
-	aliasName = "${lambdas_alias_name}",
-	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+		lambdaName = "hello_world",
+		roleName = "hello_world-role",
+		isPublishVersion = true,
+		aliasName = "${lambdas_alias_name}",
+		logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
 public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
 
+	private static final ObjectMapper objectMapper = new ObjectMapper();
+
+	@Override
 	public Map<String, Object> handleRequest(Object request, Context context) {
-		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("message", "Hello from Lambda");
-		return resultMap;
+		System.out.println("Hello from Lambda");
+
+		// Return the expected format directly
+		Map<String, Object> response = new HashMap<>();
+		response.put("statusCode", 200);
+		response.put("message", "Hello from Lambda");
+
+		return response;
 	}
+
 }
